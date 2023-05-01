4dce8dced3ad3d2e8be4ec4fdafffdb57e767a13
David Wu <david6906817@gmail.com>
2023-05-02 00:20:36 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Write};
+use std::error;
+
+mod scan;
+
+type Result = std::result::Result<(), Box<dyn error::Error>>;
+
+// fn run_file(path: &str) -> Result {
+//
+// }
+
+fn run_prompt() -> Result {
+    let mut buf = String::new();
+    let input = io::stdin();
+    let mut output = io::stdout().lock();
+
+    loop {
+        output.write(b"> ")?;
+        output.flush()?;
+
+        input.read_line(&mut buf)?;
+
+        output.write(format!("Enter content: {}", buf).as_bytes())?;
+
+        buf.clear();
+    }
+}
+
+fn main() -> Result {
+    run_prompt()
+}
